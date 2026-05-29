9d30b7967f3471ce0b6c4d832791ca4032318e84
Dilnoor Singh <dsingh6_be25@thapar.edu>
2026-05-29 15:41:33 +0530

    basics-even odd checker

---
+++ even_odd.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cout << "Enter a number: ";
+    cin >> n;
+    
+    if(n % 2 == 0){
+        cout << n << " is Even" << endl;
+    } else {
+        cout << n << " is Odd" << endl;
+    }
+    
+    return 0;
+}
